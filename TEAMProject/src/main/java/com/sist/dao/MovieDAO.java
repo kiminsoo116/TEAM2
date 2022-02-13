6961f850ecf2b30a43ea9bf51b5772e411163cc3
megic <megic@LAPTOP-DIGOE0HN>
2022-02-13 16:12:44 +0900

    123214112

--- TEAMProject/src/main/java/com/sist/dao/MovieDAO.java
+++ TEAMProject/src/main/java/com/sist/dao/MovieDAO.java
@@ -130,44 +130,4 @@ public class MovieDAO {
 			}
 		return list;
 	}
-	
-	public List<MovieVO> movieListData2(){
-		List<MovieVO> list=new ArrayList<MovieVO>();
-		try {
-			getConnection();
-			String sql="SELECT m_no,m_title,m_poster,m_rate "
-					+ "FROM movie "
-					+ "WHERE m_rdate  LIKE '2022 .03%' "
-					+ "OR m_rdate LIKE '2022 .04%' "
-					+ "OR m_rdate LIKE '2022 .05%' "
-					+ "OR m_rdate LIKE '2022 .06%' "
-					+ "OR m_rdate LIKE '2022 .07%' "
-					+ "OR m_rdate LIKE '2022 .08%' "
-					+ "OR m_rdate LIKE '2022 .09%' "
-					+ "OR m_rdate LIKE '2022 .10%' "
-					+ "OR m_rdate LIKE '2022 .11%' "
-					+ "OR m_rdate LIKE '2022 .12%' "
-					+ "ORDER BY m_no ASC";
-			
-			ps=conn.prepareStatement(sql);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next()) {
-				MovieVO vo=new MovieVO();
-				vo.setM_no(rs.getInt(1));
-				vo.setM_title(rs.getString(2));
-				vo.setM_poster(rs.getString(3));
-				vo.setM_rate(rs.getString(4));
-				
-				list.add(vo);
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			disConnection();
-		}
-		
-		return list;
-	}
-	
 }
